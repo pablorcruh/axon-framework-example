a31a3a51673a0e5d4ed3d1c2ae23a98af082c9fc
pcruz <pablo.cruz@ppm.com.ec>
2021-06-11 09:27:35 -0500

    change annotation on command classes

--- user-cmd/src/main/java/ec/com/pablorcruh/usercmd/commands/RemoveUserCommand.java
+++ user-cmd/src/main/java/ec/com/pablorcruh/usercmd/commands/RemoveUserCommand.java
@@ -1,11 +1,11 @@
 package ec.com.pablorcruh.usercmd.commands;
 
 import lombok.Data;
-import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Data
 public class RemoveUserCommand {
 
-    @AggregateIdentifier
+    @TargetAggregateIdentifier
     private String id;
 }
--- user-cmd/src/main/java/ec/com/pablorcruh/usercmd/commands/UpdateUserCommand.java
+++ user-cmd/src/main/java/ec/com/pablorcruh/usercmd/commands/UpdateUserCommand.java
@@ -3,13 +3,13 @@ package ec.com.pablorcruh.usercmd.commands;
 import com.example.usercore.models.User;
 import lombok.Builder;
 import lombok.Data;
-import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Data
 @Builder
 public class UpdateUserCommand {
 
-    @AggregateIdentifier
+    @TargetAggregateIdentifier
     private String id;
 
     private User user;
