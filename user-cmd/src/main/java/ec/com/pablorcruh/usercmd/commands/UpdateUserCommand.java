1dba4c444b510c13362abf177e57ddb6f3d416ae
pcruz <pablo.cruz@ppm.com.ec>
2021-06-11 18:37:56 -0500

    add REST controller for updating user

--- user-cmd/src/main/java/ec/com/pablorcruh/usercmd/commands/UpdateUserCommand.java
+++ user-cmd/src/main/java/ec/com/pablorcruh/usercmd/commands/UpdateUserCommand.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 public class UpdateUserCommand {
@@ -12,5 +15,7 @@ public class UpdateUserCommand {
     @TargetAggregateIdentifier
     private String id;
 
+    @NotNull(message = "no user details were supplied")
+    @Valid
     private User user;
 }
---
+++ user-cmd/src/main/java/ec/com/pablorcruh/usercmd/controllers/UpdateUserController.java
@@ -0,0 +1,39 @@
+package ec.com.pablorcruh.usercmd.controllers;
+
+import ec.com.pablorcruh.usercmd.commands.UpdateUserCommand;
+import ec.com.pablorcruh.usercmd.dtos.BaseResponse;
+import ec.com.pablorcruh.usercmd.dtos.RegisterUserResponse;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path = "/api/v1/updateUser")
+public class UpdateUserController {
+
+
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public UpdateUserController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> updateUser(@PathVariable(value = "id") String id, @Valid @RequestBody UpdateUserCommand command){
+        try{
+            command.setId(id);
+            commandGateway.send(command);
+            return new ResponseEntity<>(new BaseResponse("user updated successfully"), HttpStatus.OK);
+        }catch(Exception e){
+            var safeErrorMessage = "Error while proccessing update user request for id: " + id;
+            System.out.println(e.toString());
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
