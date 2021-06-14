541aac52cedfbdb340895035200e4b650d8719f9
pcruz <pablo.cruz@ppm.com.ec>
2021-06-14 11:42:49 -0500

    add Authorization server and web  security config

---
+++ oauth-server/src/main/java/ec/com/pablorcruh/oauthserver/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package ec.com.pablorcruh.oauthserver.repositories;
+
+import com.example.usercore.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    @Query("{'account.username': ?0}")
+    Optional<User> findByUsername(String username);
+}
